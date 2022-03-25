42777bcd2dab6ee4b29bac9b3b8a73d005f8352a
Caminha <gabrielcaminha255@gmail.com>
2022-03-25 01:37:50 -0300

    2 tests added to function at day 9.3 fixation exercise

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -2,5 +2,18 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
-  it('', () => {});
+  it('Testa se o nome da personagem com id "720" é "Wonder Woman', async () => {
+    const character = await fetchCharacter('720');
+    const name = character.name;
+    const expected = 'Wonder Woman';
+    expect.assertions(1);
+    expect(name).toBe(expected);
+  });
+
+  it('Testa se a função retorna erro ao não receber parâmetros', async () => {
+    const response = await fetchCharacter();
+    const error = new Error("You must provide an url");
+    expect.assertions(1);
+    expect(response).toEqual(error);
+  });
 });
